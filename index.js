9288b8178b9e563b661ceee5c6a19f897cc18c23
William Lim <manvinedi@gmail.com>
2019-11-03 22:26:26 +0800

    added functions

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 exports.camelCase = function (test) {
   return test
 }
+
+function camelCaseToDash (str) {
+  return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+}
+
+function toTitleCase (str) {
+  return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() })
+}
+
+function removeDashes (str) {
+  return str.replace(/-/g, ' ')
+}
